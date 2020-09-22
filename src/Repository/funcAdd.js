3d402da542de3b2c13974ff8821ec92d5f402f17
Pedro <pedrodruida1998@gmail.com>
2020-09-22 20:50:23 -0300

    atualizacao com codigos de post inacabados

--- src/Repository/funcAdd.js
+++ src/Repository/funcAdd.js
@@ -1,8 +1,9 @@
-import React,{useState, useEffect} from 'react';
+import React,{useState,Component, useEffect} from 'react';
 import '../App.css';
 import styled from 'styled-components';
 import api from '../Services/api.js';
 import { render } from '@testing-library/react';
+import axios from 'axios';
 
 const Button = styled.a`
 text-decoration: none;
@@ -19,13 +20,98 @@ transition: 0.5s all ease-out;
 background-color: Blue;
 color: white;
 }`
-function FuncAdd() {
+
+class FuncAdd extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+            id: '',
+            nome: '',
+            cpf: '',
+		}
+	}
+
+	changeHandler = e => {
+		this.setState({ [e.target.name]: e.target.value })
+	}
+
+	submitHandler = e => {
+		e.preventDefault()
+		console.log(this.state)
+		api.post('/funcionario/')
+			.then(response => {
+				console.log(response)
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}
+
+	render() {
+		const { id,nome,cpf} = this.state
+		return (
+			<div>
+                 <Button  href='/'>Menu</Button>
+				<form onSubmit={this.submitHandler}>
+                <div>
+						<input
+							type="text"
+							name="id"
+							value={id}
+							onChange={this.changeHandler}
+						/>
+					</div>
+                    
+					<div>
+						<input
+							type="text"
+							name="nome"
+							value={nome}
+							onChange={this.changeHandler}
+						/>
+					</div>
+                    <div>
+						<input
+							type="text"
+							name="cpf"
+							value={cpf}
+							onChange={this.changeHandler}
+						/>
+					</div>
+					
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		)
+    }
+}
+export default FuncAdd;
+
+
+
+
+
+
+
+//SEGUNDO CODIGO USANDO FUNCION
+
+
+
+
+
+
+/*function FuncAdd() {
 
     const[data,setData] = useState([])
     const[id,setId] = useState('')
+    const[cpf,setCpf] = useState('')
+    const[nome,setNome] = useState('')
     var idTemp = 0
+    var cpfTemp = 0
+    var nomeTemp = 0
     useEffect(() => {
-       api.post(`/funcionario/${id}`)
+       api.post(`/funcionario/`)
     
       .then(response => {
         console.log(response)
@@ -34,45 +120,77 @@ function FuncAdd() {
       })
       .catch (error => {
         console.log(error)
-        alert ("Usuario não encontrado");
+        alert ("Não foi possivel cadastrar");
       
       })
   
-    },[id])
+    },[id,nome,cpf])
   
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      setId(idTemp)
-   
+    const submitHandler = e => {
+		e.preventDefault()
+      setCpf(cpfTemp)
+        console.log()
     }
   
-    const handleChange = (e) => {
-      idTemp =e.target.value
-      console.log(idTemp)
+    const changeHandlerCpf = e => {
+        cpfTemp = ({ [e.target.cpf]: e.target.value })
+        console.log(cpfTemp)    
     }
-  
+    const changeHandlerId = e => {
+        idTemp = ({ [e.target.id]: e.target.value })
+        console.log(idTemp)   
+    }
+    const changeHandlerNome = e => {
+        nomeTemp = ({ [e.target.nome]: e.target.value })
+        console.log(nomeTemp)        
+    }
+    
     return (
       <div>
            <div className="App">
               <br></br>
               <Button  href='/'>Menu</Button>
               <div>
-                <form onSubmit= {handleSubmit}>
-                  <h2 for="buscador">Buscar funcionário por ID</h2>
-                  <input type="text" onChange = {handleChange} id="id" name="id" inclinename="buscaId" placeholder="Inserir o numero do ID"/>
-                  <button type='submit'> Pesquisar </button>
-                </form>
+              <form onSubmit={submitHandler}>
+              <div>
+						<input
+							type="text"
+                            name="id"
+                            
+                            onChange={changeHandlerId}
+                            placeholder="Inserir o numero do ID"
+						/>
+					</div>
+                    
+					<div>
+						<input
+							type="text"
+                            name="nome"
+                           
+                            onChange={changeHandlerNome}
+                            placeholder="Inserir o nome do funcionario"
+						/>
+					</div>
+                    <div>
+						<input
+							type="text"
+                            name="cpf"
+                            cpf ="cpf"
+                            onChange={changeHandlerCpf}
+                            placeholder="Inserir o numero do CPF"
+						/>
+					</div>
+					
+					<button type="submit">Enviar</button>
+				</form>
+                
               </div>
-              <h1>
-                {data.nome}
-              </h1>
-              <h2>{data.cpf}</h2>
+        
           </div>
       </div>
     );
   }
-  
-  export default FuncAdd;
-  
+
+  export default FuncAdd; */
   
   
